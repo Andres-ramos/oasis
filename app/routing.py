82718bf7c5d561a9400680089e3d9bb6b4cebf14
Andres-ramos <ramosandres443@gmail.com>
2025-11-04 17:07:46 -0400

    Implemented routing module and route view

---
+++ app/routing.py
@@ -0,0 +1,75 @@
+import requests
+import json
+from requests.exceptions import RequestException
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_routes_from_graphopper(lng_a, lat_a, lng_b, lat_b):
+    BASE_URI = "https://graphhopper.com/api/1/route"
+    ALGORITHM = "alternative_route"
+    params = {
+        "key": os.getenv("GRAPHHOPPER-API-KEY") #Get from os env
+    }
+    header = {
+        "Content-Type": "application/json"
+    }
+    body = {
+        "points": [
+            [
+                lng_a,
+                lat_a
+            ],
+            [
+                lng_b,
+                lat_b
+            ]
+        ],
+        "profile": "foot",
+        "locale": "en",
+        "instructions": True,
+        "calc_points": True,
+        "points_encoded": False,
+        "algorithm": ALGORITHM
+    }
+    json_body = json.dumps(body, separators=(',', ':'))
+    try :
+        data = requests.post(BASE_URI, params=params, data=json_body, headers=header)
+    except RequestException:
+        pass 
+    try:
+        data = data.json()
+    except json.JSONDecodeError:
+        pass 
+    return data
+
+
+def compute_shadow_layer(bounding_box):
+    # TODO: Get angle of the sun using the datetime and location
+    # TODO: Do computations and stuff to get the shade layer
+    # TODO: Should probably return a multipolygon
+    return 
+
+def compute_ndvi_layer(bounding_box):
+    # TODO: Compute the ndvi from a given satelite image
+    # TODO: Static image or do we compute ndvi on the fly with the most recent image?
+    # TODO: Should probably return a multipolygon
+    return 
+
+def get_route(lng_a, lat_a, lng_b, lat_b):
+    payload = get_routes_from_graphopper(lng_a, lat_a, lng_b, lat_b)
+    return payload
+
+    # TODO: Implement algorithm for computing overlaps with shadow and ndvi layers and return best route
+    # TODO: Get bounding box from the payload
+    bbox = ""
+    shadow_layer = compute_shadow_layer(bbox)
+    ndvi_layer = compute_ndvi_layer(bbox)
+
+    best_route = ""
+    for route in payload["route"]:
+        # TODO: Measure overlap with shadow layer and ndvi layer
+        pass 
+
+    return best_route
--- app/views.py
+++ app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json 
+from django.http import JsonResponse
+from .routing import get_route
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@require_POST
+@csrf_exempt
+def route(request):
+
+    try:
+        data = json.loads(request.body)
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'Invalid JSON'}, status=400)
+    
+    origin = data.get('origin')
+    destination = data.get('destination')
+    if not origin:
+        return JsonResponse({'error': 'origin is required'}, status=400)
+    if not destination:
+        return JsonResponse({'error': 'destination is required'}, status=400)
+    
+    route = get_route(
+        origin['lng'], origin['lat'],
+        destination['lng'], destination['lat']
+    )
+
+    return JsonResponse({'route': route})
