09d4caf060d95ee8a7c85ce53a2c50193eebc703
Andres-ramos <ramosandres443@gmail.com>
2025-12-10 11:54:43 -0400

    Woops

--- app/admin.py
+++ app/admin.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from .models import Route
 from .models import POI
 from .models import NDVILayer
+from .models import Shadow
 
 # Register your models here.
 models = [
     NDVILayer,
     POI,
-    Route
+    Route,
+    Shadow
 ]
 admin.site.register(models)
